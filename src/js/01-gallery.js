e3426469d570f7555c82dfcfc96df59e153e1a39
LiudmylaBolotova <89790016+LiudmylaBolotova@users.noreply.github.com>
2022-02-11 16:49:41 +0200

    Добавляет ДЗ

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+const listGalleryItems = createItemsOnGallery(galleryItems);
+
+galleryEl.insertAdjacentHTML('beforeend', listGalleryItems);
+
+function createItemsOnGallery(item) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class = "gallery__item">
+      <a class = "gallery__link" href="${original}">
+      <img 
+      class ="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}">
+      </a>
+      </div>`;
+    })
+    .join(' ');
+}
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
 
+const KEY = 'feedback-form-state';
+const formFeedback = document.querySelector('.feedback-form');
+const formData = {};
+
+formFeedback.addEventListener('submit', onFormSubmit);
+formFeedback.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput(event) {
+  formData.email = formFeedback.elements.email.value;
+  formData.message = formFeedback.elements.message.value;
+  localStorage.setItem(KEY, JSON.stringify(formData));
+}
+
+updateForm();
+
+function updateForm() {
+  const savedData = localStorage.getItem(KEY);
+  if (savedData) {
+    const { email, message } = JSON.parse(savedData);
+    formFeedback.email.value = email;
+    formFeedback.message.value = message;
+    formData.email = email;
+    formData.message = message;
+  }
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formDataToSend = new FormData(event.currentTarget);
+  formDataToSend.forEach((value, name) => {
+    formData[name] = value;
+  });
+
+  event.currentTarget.reset();
+  localStorage.removeItem(KEY);
+
+  console.log(formData);
+}
